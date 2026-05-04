0c420045e4371294ab68456434b8e7e7894d9fff
minh-mn <23130191@st.hcmuaf.edu.vn>
2026-05-04 22:29:01 +0700

    Control Snake

--- src/controller/InputHandler.java
+++ src/controller/InputHandler.java
@@ -1,19 +1,48 @@
 package controller;
+
+import model.Direction;
+
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class InputHandler extends KeyAdapter {
-    private GameController controller;
+    private final GameController controller;
+    private int currentKeyCode;
 
     public InputHandler(GameController controller) {
         this.controller = controller;
     }
 
-
     @Override
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-            controller.startGame();
+    public void keyPressed(KeyEvent event) {
+        currentKeyCode = event.getKeyCode();
+
+        if (currentKeyCode == KeyEvent.VK_ENTER) {
+            controller.handleStartOrRestartRequest();
+            return;
         }
+
+        Direction newDirection = mapKeyToDirection(currentKeyCode);
+        if (newDirection != null) {
+            controller.requestChangeDirection(newDirection);
+        }
+    }
+
+    public Direction mapKeyToDirection(int keyCode) {
+        return switch (keyCode) {
+            case KeyEvent.VK_UP, KeyEvent.VK_W -> Direction.UP;
+            case KeyEvent.VK_DOWN, KeyEvent.VK_S -> Direction.DOWN;
+            case KeyEvent.VK_LEFT, KeyEvent.VK_A -> Direction.LEFT;
+            case KeyEvent.VK_RIGHT, KeyEvent.VK_D -> Direction.RIGHT;
+            default -> null;
+        };
+    }
+
+    public boolean isValidKey(int keyCode) {
+        return mapKeyToDirection(keyCode) != null;
+    }
+
+    public int getCurrentKeyCode() {
+        return currentKeyCode;
     }
 }

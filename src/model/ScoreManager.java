468dcee997b17b31a40b6c843800ea4183eaa3f7
Châu Nguyễn Nhật Anh <23130006@st.hcmuaf.edu.vn>
2026-05-06 20:14:23 +0700

    Add high score tracking to ScoreManager

--- src/model/ScoreManager.java
+++ src/model/ScoreManager.java
@@ -2,10 +2,21 @@ package model;
 
 public class ScoreManager {
     private int currentScore;
+    private int highScore;
 
     public void resetScore() {
         this.currentScore = 0;
+        this.highScore = 0;
+    }
+
+ // Hàm tăng điểm khi ăn mồi
+    public void increaseScore() {
+        currentScore += 10; // Mỗi lần ăn tăng 10 điểm
+        if (currentScore > highScore) {
+            highScore = currentScore;
+        }
     }
 
     public int getCurrentScore() { return currentScore; }
+    public int getHighScore() { return highScore; }
 }

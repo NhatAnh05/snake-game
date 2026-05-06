5d64332b16e3b220984a3df55dec30c17cdcfd13
Châu Nguyễn Nhật Anh <23130006@st.hcmuaf.edu.vn>
2026-05-06 20:14:08 +0700

    Refactor Point constructor to use super class

--- src/model/Point.java
+++ src/model/Point.java
@@ -1,6 +1,7 @@
 package model;
 
 public class Point extends java.awt.Point {
-    public int x, y;
-    public Point(int x, int y) { this.x = x; this.y = y; }
+	public Point(int x, int y) {
+        super(x, y); 
+    }
 }

c89b9a893b79bd2f0e035fd6a56d04756211b28b
NhatAnh05 <23130006@st.hcmuaf.edu.vn>
2026-05-29 14:22:37 +0700

    refactor(ScoreManager): Áp dụng Multi-threading để lưu điểm

--- snake-game-main/src/model/ScoreManager.java
+++ snake-game-main/src/model/ScoreManager.java
@@ -1,4 +1,42 @@
 package model;
 
 public class ScoreManager {
+    private int currentScore;
+    private int highScore;
+
+    public ScoreManager() {
+        this.currentScore = 0;
+        this.highScore = HighScoreRepository.loadHighScore();
+    }
+
+    public void resetScore() {
+        this.currentScore = 0;
+        this.highScore = HighScoreRepository.loadHighScore();
+    }
+
+    // Hàm tăng điểm khi ăn mồi
+    public void addScore() {
+        currentScore += 10;
+
+        if (currentScore > highScore) {
+            highScore = currentScore;
+
+            // [Nâng cấp UC05] - Nhat Anh
+            // Đẩy tiến trình ghi I/O ổ cứng sang luồng ngầm (Background Thread)
+            // Giúp Game Loop (Timer) không bị chờ, chống hiện tượng khựng (lag) game.
+            final int scoreToSave = highScore; // Tạo bản sao final để truyền vào Thread cho an toàn
+
+            new Thread(() -> {
+                HighScoreRepository.saveHighScore(scoreToSave);
+            }).start();
+        }
+    }
+
+    public int getCurrentScore() {
+        return currentScore;
+    }
+
+    public int getHighScore() {
+        return highScore;
+    }
 }

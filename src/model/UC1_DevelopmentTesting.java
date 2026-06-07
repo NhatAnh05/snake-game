47b386b9605e02887e04a0239f69581c23220e47
BuiThiPhuongAnh_23130005 <23130005@st.hcmuaf.edu.vn>
2026-06-07 18:21:56 +0700

    Thêm file UC1_DevelopmentTesting.java để thực hiện Development Testing tập trung cho Use Case 1

--- src/model/UC1_DevelopmentTesting.java
+++ src/model/UC1_DevelopmentTesting.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-    public class UC1_ReleaseTesting  {
+    public class UC1_DevelopmentTesting {
 
         private GameModel gameModel;
         private Snake snake;
